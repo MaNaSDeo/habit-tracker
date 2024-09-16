952c29b19294c0e59cfc13be71c3a3a71ccf361d
manasdeo <rmnslogin@gmail.com>
2024-09-16 12:45:26 +0530

    exporting state and dispatch type

--- src/Features/Tracker/store.ts
+++ src/Features/Tracker/store.ts
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import habitReducer from "./habitSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     habits: habitReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export default store;
